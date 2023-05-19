ada822dc3dad23504e627a337451ee6294d3c816
Jeddy Certifico <jeddycolon.certifico@bicol-u.edu.ph>
2023-05-19 09:52:33 +0800

    first commit

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const io = require("socket.io")(server);
 const PORT = process.env.PORT || 8000;
 const url = "https://chat-app-9ke7.onrender.com";
 
+// var username = "";
+const usernames = {};
 var username = "";
 const rooms = { Main: { users: {} } };
 
@@ -30,7 +32,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/room", checkUserName, (req, res) => {
-  res.render("pages/room", { rooms: rooms });
+  res.render("pages/room", { rooms: rooms, name: username });
 });
 
 app.post("/welcome", (req, res) => {
@@ -65,10 +67,17 @@ function checkUserName(req, res, next) {
 
 // io connection
 io.on("connection", (socket) => {
+  // enter room page
+  socket.on("enter-room", (name) => {
+    usernames[socket.id] = name;
+    console.log(`${name} entered the room in ${socket.id}`);
+  });
+
   // joining
   socket.on("join", (room) => {
     socket.join(room);
     rooms[room].users[socket.id] = username;
+    console.log(`${username} joined the chat in ${room} with id ${socket.id}`);
     socket.broadcast.to(room).emit("join", username, () => {
       username = "";
       if (rooms[room].users[socket.id]) console.log(`${rooms[room].users[socket.id]} joined the chat in ${room}`);
